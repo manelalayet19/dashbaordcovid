eb06540834d5f41afdc90170126c7b501003b7d9
manelalayet19 <alayet.manel@gmail.com>
2022-11-19 13:55:28 -0500

    1

--- app.py
+++ app.py
@@ -2,17 +2,29 @@ import dash
 import dash_bootstrap_components as dbc
 #import dash_auth
 import random
+import flask
+from flask import send_from_directory
 import os
+
+# define password login  form auth
+# USERNAME_PASSWORD_PAIRS=[['username','password']
+# auth= dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
+
 # bootstrap theme
-external_stylesheets = [dbc.themes.QUARTZ]
+external_stylesheets = [dbc.themes.QUARTZ, dbc.icons.FONT_AWESOME]
 # meta_tags are required for the app layout to be mobile responsive
 app = dash.Dash(__name__, assets_external_path='assets/style.css',
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
 
-# define password login  form auth
-# USERNAME_PASSWORD_PAIRS=[['username','password']
-# auth= dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS) #authentification per person
 server = app.server
 app.config.suppress_callback_exceptions = True
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
+
+
+# @app.server.route('/assests/<path:path>')
+# def static_file(path):
+#     static_folder = os.path.join(os.getcwd(), 'assests')
+#     return send_from_directory(static_folder, path)
